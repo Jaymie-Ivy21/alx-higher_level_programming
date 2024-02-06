62607620ebb1b70277df54d28446513fc2ae3ce9
James <oluwajamie@gmail.com>
2024-02-06 05:25:49 -0800

    Coordinates of a square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
-"""an empty class Square that defines a square"""
+"""An empty class Square that defines a square."""
 
 class Square:
-    
-    """Square attributes are defined here"""
-    
+    """This represent a square."""
     pass
